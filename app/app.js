c222ab5cc57c0c9d3f241a6c50c14467b6350196
Dave Zwieback <dz@mindweather.com>
2014-11-14 15:01:26 -0500

    initial commit.

--- app/app.js
+++ app/app.js
@@ -7,6 +7,27 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version'
 ]).
+
+angular.module("myApp", ["firebase"]);
+
+angular.module('myApp', ['firebase'])
+.controller("MyCtrl", ["$scope", "$firebase",
+  function($scope, $firebase) {
+    // Our controller definition goes here
+  }
+]);
+
+angular.module('myApp')
+.controller("MyCtrl", function($scope, $firebase) {
+  // Firebase URL
+  var URL = "https://nbs.firebase.com";
+  // Synchronizing the items on our $scope
+  $scope.items = $firebase(new Firebase(URL + '/items'));
+});
+
+$scope.items.hello = "baz";
+$scope.items.$save("hello");  // new Firebase(URL + "/foo") now contains "baz".
+
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
